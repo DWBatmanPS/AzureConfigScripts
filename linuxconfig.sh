d454e5cbe8f3ee353b7e0d1d37d61d03a8e2dada
DWBatmanPS <96199880+DWBatmanPS@users.noreply.github.com>
2025-03-07 16:22:12 -0600

    Update linuxconfig.sh

--- linuxconfig.sh
+++ linuxconfig.sh
@@ -3,7 +3,6 @@ while [[ ! -z "$(ps aux | grep 'apt-get update' | grep -v 'grep')" ]]; do
     echo "- [$(date '+%F-%H-%M-%S')] apt-get update is still running: [$(ps aux | grep 'apt-get update' | grep -v 'grep')]" >> /init_log
     sleep 10
 done
-sudo apt-get -y update
-sudo apt-get -y upgrade 
+sudo apt-get update 
 sudo apt-get -y install python3-pip 
 sudo apt -y install python3-azure 
